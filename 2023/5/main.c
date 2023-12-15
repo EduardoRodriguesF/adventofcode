a8ebc5a07459f86a02e59fbf6aec8bae3e316e21
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-12-15 13:45:11 -0300

    day 5: possibly solved part 2, but unbearably slow

--- 2023/5/main.c
+++ 2023/5/main.c
@@ -5,63 +5,82 @@
 #include <stdbool.h>
 #include <string.h>
 
-int main() {
-    size_t bufsize = 1024;
-    char* line;
+size_t translate(size_t seed, char** almanac_lines, size_t n);
 
-    size_t seeds[128];
-    size_t n = 0;
+int main() {
+    size_t bufsize = 256;
+    char* seed_line = malloc(bufsize);
+    char* line = malloc(bufsize);
+    char** lines = malloc(bufsize * sizeof(char*));
+    size_t lines_num = 0;
 
     // Get first line, defining seeds
-    if (getline(&line, &bufsize, stdin) == 0) return 1;
-    char* token = strtok(line, " ");
+    if (getline(&seed_line, &bufsize, stdin) == 0) return 1;
+    while (!isdigit(*seed_line)) seed_line++;
+
+    while (getline(&line, &bufsize, stdin) > 0) {
+        lines[lines_num] = malloc(bufsize);
+        strcpy(lines[lines_num], line);
+
+        lines_num++;
+    }
+
+    char* saved;
+    char* token = strtok_r(seed_line, " ", &saved);
+    size_t pair_seed;
+    size_t i = 0;
+    size_t lowest = 0;
 
     while (token != NULL) {
-        if (isdigit(*token)) {
-            seeds[n++] = atoi(token);
-        }
+        if ((++i) % 2 != 0) {
+            pair_seed = atoi(token);
 
-        token = strtok(NULL, " ");
-    }
-
-    bool seeds_moved[n];
-
-    // Process movements
-    while (getline(&line, &bufsize, stdin) > 0) {
-        if (*line == '\n') {
-            // Reset flags
-            for (int i = 0; i < n; i++) seeds_moved[i] = false;
-            continue;
-        }
-
-        if (isdigit(*line)) {
-
-            size_t dest_start = atoi(strtok(line, " "));
-            size_t src_start = atoi(strtok(NULL, " "));
-            size_t range = atoi(strtok(NULL, " "));
-
-            size_t src_end = src_start + range - 1;
-
-            for (size_t i = 0; i < n; i++) {
-                size_t seed = seeds[i];
-
-                if (!seeds_moved[i] && seed >= src_start && seed <= src_end) {
-                    size_t offset = seed - src_start;
-                    seeds[i] = dest_start + offset;
-                    seeds_moved[i] = true;
-                }
+            size_t seed = translate(pair_seed, lines, lines_num);
+            if (lowest == 0 || seed < lowest) lowest = seed;
+        } else {
+            size_t range = atoi(token);
+            for (size_t j = 1; j < range; j++) {
+                printf("%zu out of %zu\n", j, range);
+                size_t seed = translate(pair_seed + j, lines, lines_num);
+                if (seed < lowest) lowest = seed;
             }
-
-            continue;
         }
-    }
 
-    size_t lowest = seeds[0];
-    for (size_t i = 1; i < n; i++) {
-        if (seeds[i] < lowest) lowest = seeds[i];
+        token = strtok_r(NULL, " ", &saved);
     }
 
     printf("Lowest: %zu\n", lowest);
 
+    free(lines);
+    free(line);
     return 0;
+}
+
+size_t translate(size_t seed, char** almanac_lines, size_t n) {
+    bool has_moved = false;
+
+    char* line = malloc(256);
+
+    for (size_t i = 0; i < n; i++) {
+        strcpy(line, almanac_lines[i]);
+        char* saved;
+
+        if (has_moved || !isdigit(*line)) {
+            if (*line == '\n') has_moved = false;
+            continue;
+        }
+
+        size_t dest_start = atoi(strtok_r(line, " ", &saved));
+        size_t src_start = atoi(strtok_r(NULL, " ", &saved));
+        size_t range = atoi(strtok_r(NULL, " ", &saved));
+
+        size_t src_end = src_start + range - 1;
+
+        if (seed >= src_start && seed <= src_end) {
+            seed = dest_start + (seed - src_start);
+            has_moved = true;
+        }
+    }
+
+    return seed;
 }
