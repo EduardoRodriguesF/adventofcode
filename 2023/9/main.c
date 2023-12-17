a9d13a861cdc6bcd740e08711a84babc2db410af
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-12-17 19:14:53 -0300

    day 9: solve part 2

--- 2023/9/main.c
+++ 2023/9/main.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void reverse(int arr[], int n);
+
 int main() {
     size_t bufsize = 256;
     char* line = malloc(bufsize);
@@ -21,10 +23,20 @@ int main() {
             token = strtok_r(NULL, " ", &save);
         }
 
+        reverse(sequence, n);
         if (n > 0) result += predict_next(sequence, n);
     }
 
     printf("%zu\n", result);
 
     return 0;
+}
+
+void reverse(int arr[], int n) {
+    int i, j;
+    for (i = 0, j = n - 1; i < j; i++, j--) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
