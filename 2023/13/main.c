3d3734dd9c5200508c6d3cbb100debafb77a81e2
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-12-28 22:39:29 -0300

    solve 13-2
    Squashed commit of the following:
    
    commit f77132495dd3f9ffda40aa2c16d1fe2349d108ea
    Author: EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
    Date:   Thu Dec 28 22:39:14 2023 -0300
    
        fix_smudge makes all the effort
    
    commit 90cf1050cd90209a1019f54cbf1604f6825f9ff5
    Author: EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
    Date:   Thu Dec 28 22:32:06 2023 -0300
    
        fix smudge, but not solved

--- 2023/13/main.c
+++ 2023/13/main.c
@@ -5,10 +5,20 @@
 
 #define MAX_ROWS 512
 
+#define ASH '.'
+#define ROCK '#'
+
+#define REV(c) (c == ASH ? ROCK : ASH)
+
 typedef struct grid {
     char** rows;
     size_t w, h;
 } Grid;
+
+typedef enum reflection_t {
+    Vertical,
+    Horizontal,
+} ReflectionType;
 
 void push_row(Grid* grid, char* data) {
     int i = 0;
@@ -23,11 +33,13 @@ void push_row(Grid* grid, char* data) {
     grid->h++;
 }
 
-int find_horizontal_reflection_index(const Grid* grid) {
+int find_horizontal_reflection_index(const Grid* grid, int ignore) {
     int a, b, target;
     bool match;
 
     for (target = 0; target < grid->h - 1; target++) {
+        if (target == ignore) continue;
+
         match = true;
         a = target;
         b = target + 1;
@@ -47,11 +59,13 @@ int find_horizontal_reflection_index(const Grid* grid) {
     return -1;
 }
 
-int find_vertical_reflection_index(const Grid* grid) {
+int find_vertical_reflection_index(const Grid* grid, int ignore) {
     int a, b, target;
     bool match;
 
     for (target = 0; target < grid->w - 1; target++) {
+        if (target == ignore) continue;
+
         match = true;
         a = target;
         b = target + 1;
@@ -75,6 +89,45 @@ int find_vertical_reflection_index(const Grid* grid) {
     return -1;
 }
 
+int fix_smudge(Grid* grid) {
+    int prev;
+    ReflectionType dir;
+    int (*samedir_fn)(const Grid*, int);
+    int (*opposite_fn)(const Grid*, int);
+    if ((prev = find_vertical_reflection_index(grid, -1)) != -1) {
+        dir = Vertical;
+        samedir_fn = find_vertical_reflection_index;
+        opposite_fn = find_horizontal_reflection_index;
+    } else {
+        dir = Horizontal;
+        prev = find_horizontal_reflection_index(grid, -1);
+        samedir_fn = find_horizontal_reflection_index;
+        opposite_fn = find_vertical_reflection_index;
+    }
+
+    for (int i = 0; i < grid->h; i++) {
+        char* row = grid->rows[i];
+
+        for (int col = 0; col < grid->w; col++) {
+            row[col] = REV(row[col]);
+
+            int v;
+            if ((v = opposite_fn(grid, -1)) != -1) {
+                return (v+1) * (dir == Vertical ? 100 : 1);
+            };
+
+            if ((v = samedir_fn(grid, prev)) != -1 && v != prev) {
+                return (v+1) * (dir == Horizontal ? 100 : 1);
+            }
+            // Back to normal
+            row[col] = REV(row[col]);
+        }
+    }
+
+    fprintf(stderr, "could not find smudge\n");
+    exit(1);
+}
+
 int main() {
     size_t bufsize = 128;
     char* line = malloc(bufsize);
@@ -92,18 +145,10 @@ int main() {
             continue;
         }
 
-        int reflection, n;
-        n = 0;
-        if ((reflection = find_vertical_reflection_index(&grid)) != -1) {
-            n = reflection + 1;
-        } else if ((reflection = find_horizontal_reflection_index(&grid)) != -1) {
-            n = (reflection + 1) * 100;
-        }
+        result += fix_smudge(&grid);
 
         grid.h = 0;
         grid.w = 0;
-
-        result += n;
     } while (line_length > 0);
 
     printf("%ld\n", result);
